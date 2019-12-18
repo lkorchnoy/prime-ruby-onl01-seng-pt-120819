fc0399199356b215d578e06521a06422fdf91314
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-18 16:20:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+def prime(n)
+  Prime.prime?(n)
+  "include {:focus=>true}"
+end
+prime(2)
+
+def prime(n)
+  Prime.prime?(n)
+  "include {:focus=>true}"
+end
+prime(-1)
+
+def prime(nums)
+  nums.collect { |n| n/1. n/n }
+end
+prime(2)
+  
+
+#numbers = [2, 3, 5, 7, 11]
+#numbers.select do |n|
+#n.prime?
+#"include {:focus=>true}"
